88a05f06a2b4b1cd84eba62cb9ca8c089d224275
chocochaii <thanat.apiwattanadej@gmail.com>
2020-02-18 17:06:32 +0700

    [Added] mock-up database

---
+++ db.js
@@ -0,0 +1,434 @@
+var lorem =
+	'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ornare sapien a nisi egestas lacinia. Donec nibh felis, luctus eget dolor ac, aliquet vulputate tellus. Donec nec pretium tellus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.'
+
+module.exports = {
+	beers: [
+		{
+			id: 1,
+			title: 'Singha',
+			description: lorem,
+			style_id: 1,
+			country_id: 1,
+			image_path: 'https://via.placeholder.com/300'
+		},
+		{
+			id: 2,
+			title: 'Chang',
+			description: lorem,
+			style_id: 1,
+			country_id: 1,
+			image_path: 'https://via.placeholder.com/300'
+		},
+		{
+			id: 3,
+			title: 'Leo',
+			description: lorem,
+			style_id: 1,
+			country_id: 1,
+			image_path: 'https://via.placeholder.com/300'
+		},
+		{
+			id: 4,
+			title: 'Hoegaarden',
+			description: lorem,
+			style_id: 2,
+			country_id: 3,
+			image_path: 'https://via.placeholder.com/300'
+		},
+		{
+			id: 5,
+			title: 'Speedway Stout',
+			description: lorem,
+			style_id: 3,
+			country_id: 2,
+			image_path: 'https://via.placeholder.com/300'
+		},
+		{
+			id: 6,
+			title: "The Jubilant Operation Pale",
+			description: lorem,
+			style_id: 1,
+			country_id: 2,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 7,
+			title: "The Silversmith Pilsner",
+			description: lorem,
+			style_id: 3,
+			country_id: 2,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 8,
+			title: "Milkshake County Porter",
+			description: lorem,
+			style_id: 2,
+			country_id: 3,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 9,
+			title: "Ignited Schoolboy Ale",
+			description: lorem,
+			style_id: 3,
+			country_id: 1,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 10,
+			title: "Pig's Granddaughter",
+			description: lorem,
+			style_id: 2,
+			country_id: 2,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 11,
+			title: "Diddy's Daybreak Witbier",
+			description: lorem,
+			style_id: 3,
+			country_id: 3,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 12,
+			title: "Anomaly Pond Pale Lager",
+			description: lorem,
+			style_id: 1,
+			country_id: 3,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 13,
+			title: "Warbler's Advertisement",
+			description: lorem,
+			style_id: 3,
+			country_id: 1,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 14,
+			title: "Liquid Corndog",
+			description: lorem,
+			style_id: 3,
+			country_id: 2,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 15,
+			title: "Dr. Anybody's Stout",
+			description: lorem,
+			style_id: 2,
+			country_id: 2,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 16,
+			title: "Liquid Theater",
+			description: lorem,
+			style_id: 1,
+			country_id: 2,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 17,
+			title: "Liquid Businessman",
+			description: lorem,
+			style_id: 1,
+			country_id: 3,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 18,
+			title: "Miss Kangaroo's Malt",
+			description: lorem,
+			style_id: 3,
+			country_id: 3,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 19,
+			title: "Condemned Hotline",
+			description: lorem,
+			style_id: 2,
+			country_id: 3,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 20,
+			title: "Siamese's Horseman",
+			description: lorem,
+			style_id: 2,
+			country_id: 1,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 21,
+			title: "Bill Clinton's Nosebleed Stout",
+			description: lorem,
+			style_id: 1,
+			country_id: 2,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 22,
+			title: "The Shrew's Tinfoil",
+			description: lorem,
+			style_id: 3,
+			country_id: 2,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 23,
+			title: "Fantastic Sunfish",
+			description: lorem,
+			style_id: 2,
+			country_id: 3,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 24,
+			title: "Captain Mongoose's Brown Ale",
+			description: lorem,
+			style_id: 3,
+			country_id: 1,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 25,
+			title: "The Overseer Ale",
+			description: lorem,
+			style_id: 2,
+			country_id: 2,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 26,
+			title: "The Lizard's Sunray Beer",
+			description: lorem,
+			style_id: 3,
+			country_id: 3,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 27,
+			title: "Chihuahua",
+			description: lorem,
+			style_id: 1,
+			country_id: 3,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 28,
+			title: "Foamy Cowhand Hefeweizen",
+			description: lorem,
+			style_id: 3,
+			country_id: 1,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 29,
+			title: "Obama's Sunbather Tripel",
+			description: lorem,
+			style_id: 3,
+			country_id: 2,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 30,
+			title: "The Climber Ale",
+			description: lorem,
+			style_id: 2,
+			country_id: 2,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 31,
+			title: "Gorgon's Newborn Pilsner",
+			description: lorem,
+			style_id: 1,
+			country_id: 2,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 32,
+			title: "Old Honeye Rye",
+			description: lorem,
+			style_id: 1,
+			country_id: 3,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 33,
+			title: "Neck Town Golden Ale",
+			description: lorem,
+			style_id: 3,
+			country_id: 3,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 34,
+			title: "Powby Cider",
+			description: lorem,
+			style_id: 2,
+			country_id: 3,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 35,
+			title: "Extra Sting Ale",
+			description: lorem,
+			style_id: 2,
+			country_id: 1,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 36,
+			title: "Pie In Mig",
+			description: lorem,
+			style_id: 1,
+			country_id: 3,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 37,
+			title: "Devil Stout",
+			description: lorem,
+			style_id: 3,
+			country_id: 1,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 38,
+			title: "Panter IPA",
+			description: lorem,
+			style_id: 2,
+			country_id: 2,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 39,
+			title: "Cascade American American IPA",
+			description: lorem,
+			style_id: 2,
+			country_id: 3,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 40,
+			title: "Mezzy Wit Hefe-Red Ale",
+			description: lorem,
+			style_id: 1,
+			country_id: 3,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 41,
+			title: "Overjoyed!",
+			description: lorem,
+			style_id: 1,
+			country_id: 2,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 42,
+			title: "The Liar's Leapfrog",
+			description: lorem,
+			style_id: 1,
+			country_id: 3,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 43,
+			title: "Gorgon's Busywork Porter",
+			description: lorem,
+			style_id: 3,
+			country_id: 3,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 44,
+			title: "Depression Stout",
+			description: lorem,
+			style_id: 2,
+			country_id: 3,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 45,
+			title: "Lyndon Johnson's Ghost Red",
+			description: lorem,
+			style_id: 2,
+			country_id: 1,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 46,
+			title: "The Shutdown Ale",
+			description: lorem,
+			style_id: 1,
+			country_id: 3,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 47,
+			title: "Glitter Isle Stout",
+			description: lorem,
+			style_id: 3,
+			country_id: 1,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 48,
+			title: "Morale-osaurus Oyster Stout",
+			description: lorem,
+			style_id: 2,
+			country_id: 2,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 49,
+			title: "Something Wicked Malt",
+			description: lorem,
+			style_id: 2,
+			country_id: 3,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 50,
+			title: "Respectful Port Ale",
+			description: lorem,
+			style_id: 1,
+			country_id: 3,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 51,
+			title: "Porcupine's Intercourse Ale",
+			description: lorem,
+			style_id: 2,
+			country_id: 1,
+			image_path: "https://via.placeholder.com/300"
+		},
+		{
+			id: 52,
+			title: "Mankind's Storekeeper",
+			description: lorem,
+			style_id: 2,
+			country_id: 1,
+			image_path: "https://via.placeholder.com/300"
+		}
+	],
+	favorite_beers: [{ id: 1, beer_id: 1 }, { id: 2, beer_id: 2 }],
+	styles: [
+		{ id: 1, name: 'Pale Lager' },
+		{ id: 2, name: 'Wheat beer' },
+		{ id: 3, name: 'Imperial Stout' }
+	],
+	countries: [
+		{ id: 1, name: 'Thailand' },
+		{ id: 2, name: 'United States' },
+		{ id: 3, name: 'Germany' }
+	]
+}
